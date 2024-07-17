353b423884650865e3ce4609acb0dfa56b2fd75f
yleaf <11785335+leavesster@users.noreply.github.com>
2024-07-17 16:17:37 +0800

    add comment

--- directory/loader.py
+++ directory/loader.py
@@ -7,13 +7,25 @@ def import_module(file_path, module_name):
     assert spec is not None, f"Could not find module at {file_path}"
     
     module = importlib.util.module_from_spec(spec)
+    
+    # 使用 relative import 需要有这个
     sys.modules[module_name] = module
 
     assert spec.loader is not None, f"Could not find loader for module {module_name}"
     spec.loader.exec_module(module)
     return module
 
+def load_module(file_path, module_name):
+    if sys.modules.get(module_name):
+        return sys.modules[module_name]
+    return import_module(file_path, module_name)
 
 if __name__ == '__main__':
-    m = import_module("./m/__init__.py", "bbb")
-    m.main()
+    # directory module 只能引用  __init__.py 文件，然后使用其中 import 的内容。
+    # 文件夹内可以任意使用 relative import
+    m1 = import_module("./m/__init__.py", "bbb")
+    m1.main()
+    m1.main()
+
+    m3 = import_module("./m/__init__.py", "bbb")
+    m3.main()
--- file/loader.py
+++ file/loader.py
@@ -7,6 +7,7 @@ def import_module(file_path, module_name):
     assert spec is not None, f"Could not find module at {file_path}"
     
     module = importlib.util.module_from_spec(spec)
+    # 如果是 file，没有相对引用，可以忽略这个，这样还能避免污染
     sys.modules[module_name] = module
 
     assert spec.loader is not None, f"Could not find loader for module {module_name}"
